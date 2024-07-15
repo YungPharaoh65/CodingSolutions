177999516b94ff7e85a9262a69bd7a2b30a75750
YungPharaoh65 <princemsimango27@gmail.com>
2024-07-15 14:52:35 +0200

    made changes in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,8 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faBriefcase, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faAddressBook } from "@fortawesome/free-regular-svg-icons";
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState(""); // State to track active link
@@ -16,60 +14,54 @@ const Navbar = () => {
 
   return (
     <nav className="l-navbar">
-
-    <div className="nav-container">
-      <Link
-        to="/Home"
-        className={`link-no-underline ${activeLink === "/Home" ? "active" : ""}`}
-        onMouseOver={() => handleSetActiveLink("/Home")}
-      >
-        <div className={`m-box m-box--44 ${activeLink === "/Home" ? "active" : ""}`}>
-          <FontAwesomeIcon icon={faHouse} className="fontawesome" />
-        </div>
-      </Link>
+      <div className="nav-container">
+        <Link
+          to="/Home"
+          className={`link-no-underline ${activeLink === "/Home" ? "active" : ""}`}
+          onClick={() => handleSetActiveLink("/Home")}
+        >
+          <div className={`m-box m-box--44 ${activeLink === "/Home" ? "active" : ""}`}>
+            <FontAwesomeIcon icon={faHouse} className="fontawesome" />
+          </div>
+        </Link>
       </div>
 
-<div className="nav-container">
-      <Link
-        to="/About"
-        className={`link-no-underline ${activeLink === "/About" ? "active" : ""}`}
-        onMouseOver={() => handleSetActiveLink("/About")}
-      >
-        <div className={`m-box m-box--45 ${activeLink === "/About" ? "active" : ""}`}>
-          <FontAwesomeIcon icon={faAddressBook} className="fontawesome" />
-        </div>
-      </Link>
+      <div className="nav-container">
+        <Link
+          to="/About"
+          className={`link-no-underline ${activeLink === "/About" ? "active" : ""}`}
+          onClick={() => handleSetActiveLink("/About")}
+        >
+          <div className={`m-box m-box--45 ${activeLink === "/About" ? "active" : ""}`}>
+            <FontAwesomeIcon icon={faAddressBook} className="fontawesome" />
+          </div>
+        </Link>
       </div>
 
-<div className="nav-container">
-      <Link
-        to="/Details"
-        className={`link-no-underline ${activeLink === "/Details" ? "active" : ""}`}
-        onMouseOver={() => handleSetActiveLink("/Details")}
-      >
-        <div className={`m-box m-box--46 ${activeLink === "/Details" ? "active" : ""}`}>
-          <FontAwesomeIcon icon={faBriefcase} className="fontawesome" />
-        </div>
-      </Link>
+      <div className="nav-container">
+        <Link
+          to="/Details"
+          className={`link-no-underline ${activeLink === "/Details" ? "active" : ""}`}
+          onClick={() => handleSetActiveLink("/Details")}
+        >
+          <div className={`m-box m-box--46 ${activeLink === "/Details" ? "active" : ""}`}>
+            <FontAwesomeIcon icon={faBriefcase} className="fontawesome" />
+          </div>
+        </Link>
       </div>
 
-<div className="nav-container">
-      <Link
-        to="/Contact"
-        className={`link-no-underline ${activeLink === "/Contact" ? "active" : ""}`}
-        onMouseOver={() => handleSetActiveLink("/Contact")}
-      >
-        <div className={`m-box m-box--47 ${activeLink === "/Contact" ? "active" : ""}`}>
-          <FontAwesomeIcon icon={faPhone} className="fontawesome" />
-        </div>
-      </Link> 
+      <div className="nav-container">
+        <Link
+          to="/Contact"
+          className={`link-no-underline ${activeLink === "/Contact" ? "active" : ""}`}
+          onClick={() => handleSetActiveLink("/Contact")}
+        >
+          <div className={`m-box m-box--47 ${activeLink === "/Contact" ? "active" : ""}`}>
+            <FontAwesomeIcon icon={faPhone} className="fontawesome" />
+          </div>
+        </Link>
       </div>
-
     </nav>
-
-    
-
-    
   );
 };
 
