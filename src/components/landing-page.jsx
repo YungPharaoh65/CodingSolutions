612a115ce3f373b7fbe600ca4d22f5a31e95f53c
YungPharaoh65 <princemsimango27@gmail.com>
2024-07-15 15:35:45 +0200

    added a pdf on landing page

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -7,7 +7,12 @@ import About from "./nav-pages/About";
 import Details from "./nav-pages/Details";
 import Contact from "./nav-pages/Contact";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faTwitter, faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import {
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 function LandingPage() {
@@ -31,15 +36,15 @@ function LandingPage() {
   }, []);
 
   const formatTime = (date) => {
-    const hrs = date.getHours().toString().padStart(2, '0');
-    const mins = date.getMinutes().toString().padStart(2, '0');
-    const secs = date.getSeconds().toString().padStart(2, '0');
+    const hrs = date.getHours().toString().padStart(2, "0");
+    const mins = date.getMinutes().toString().padStart(2, "0");
+    const secs = date.getSeconds().toString().padStart(2, "0");
     return `${hrs} : ${mins} : ${secs}`;
   };
 
   const formatDate = (date) => {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   };
@@ -47,11 +52,10 @@ function LandingPage() {
   const getDayOfCode = () => {
     const startDate = new Date(2024, 6, 1); // 1st July 2024
     const differenceInTime = currentDate.getTime() - startDate.getTime();
-    const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24)) + -15; //if i change the number it will start the countdown
+    const differenceInDays =
+      Math.floor(differenceInTime / (1000 * 3600 * 24)) + -15; //if i change the number it will start the countdown
     return differenceInDays;
   };
-
-  
 
   return (
     <Router>
@@ -75,9 +79,9 @@ function LandingPage() {
           <br />
 
           <Link to="/About" className="link-no-underline">
-            <div className="changecolor"> 
-              <p2>{getDayOfCode()} of 30 days code</p2> 
-            </div> 
+            <div className="changecolor">
+              <p2>{getDayOfCode()} of 30 days code</p2>
+            </div>
           </Link>
 
           <p className="backgroundclock">
@@ -86,44 +90,55 @@ function LandingPage() {
             </div>
           </p>
         </div>
-        
 
         {/* Blueish Box */}
         <div className="container-secondary">
-        <div className="l-details2">
-          <div className="moveboxes">
-            <a href="https://www.instagram.com/princemphomsimango/" className="m-smallbox m-smallbox--44">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
+          <div className="l-details2">
+            <div className="moveboxes">
+              <a
+                href="https://www.instagram.com/princemphomsimango/"
+                className="m-smallbox m-smallbox--44"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
 
-            <a href="https://x.com/prince28_mpho" className="m-smallbox m-smallbox--45">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
+              <a
+                href="https://x.com/prince28_mpho"
+                className="m-smallbox m-smallbox--45"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
 
-            <a href="https://www.linkedin.com/in/prince-mpho-msimango-26b3b6282/" className="m-smallbox m-smallbox--46">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
+              <a
+                href="https://www.linkedin.com/in/prince-mpho-msimango-26b3b6282/"
+                className="m-smallbox m-smallbox--46"
+              >
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
 
-            <a href="https://wa.me/message/UE6XNJVTRE7IM1 " className="m-smallbox m-smallbox--47">
-              <FontAwesomeIcon icon={faWhatsapp} />
+              <a
+                href="https://wa.me/message/UE6XNJVTRE7IM1 "
+                className="m-smallbox m-smallbox--47"
+              >
+                <FontAwesomeIcon icon={faWhatsapp} />
+              </a>
+            </div>
+
+            <p className="textdetails">phone</p>
+            <p className="number">+27 61 813 5059</p>
+            <hr className="smallerline" />
+            <p className="textdetails">email</p>
+            <p className="number">princemsimango27@gmail.com</p>
+
+            {/* PDF Download Button */}
+            <a href="/Coding.pdf" download="Information">
+              <button className="pdfbutton">Download PDF</button>
             </a>
           </div>
-
-          <p className="textdetails">phone</p>
-          <p className="number">+27 61 813 5059</p>
-          <hr className="smallerline" />
-          <p className="textdetails">email</p>
-          <p className="number">princemsimango27@gmail.com</p>
-          
-          {/* PDF Download Button */}
-          <a href="/CodingSolutions.pdf" download="CodingSolutions.pdf">
-            <button className="pdfbutton">Download PDF</button>
-          </a>
-        </div>
         </div>
 
         {/* Information Box - Section */}
-        
+
         <div className="container-section"></div>
         <div className="l-informationbox">
           <Routes>
@@ -140,4 +155,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
- 
